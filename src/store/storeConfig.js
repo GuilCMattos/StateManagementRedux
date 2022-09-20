32adb85dba5fe90341ef75624185e542bfad42af
Guilherme <mttsguil@gmail.com>
2022-09-20 12:45:28 -0300

    refec: reducer

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,39 +1,10 @@
 import {createStore, combineReducers} from 'redux'
 
+import numbersReducer from './reducers/numbers'
+
 const reducers = combineReducers({
     
-    numbers: function (state, action) {
-
-        switch(action.type) { 
-            case 'NUM_MIN_ALTERADO': 
-                return {
-                    ...state,
-                    min: action.payload
-                }
-
-            case 'NUM_MAX_ALTERADO': 
-            return { 
-                ...state,
-                max: action.payload
-            }
-
-            default:
-                return { 
-                    min: 7,
-                    max: 31
-                } 
-            
-        }
-    },
-        
-       
-    names: function(state, action) { 
-        return [
-            'Ana',
-            'Bia',
-            'Carlos'
-        ]
-    }
+    numbers: numbersReducer
 })
 
 function storeConfig() { 
